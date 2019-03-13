28cdfd1b94fcbd2d69f45752a991177c43084e24
NikitaKA <nikita.ak.85@gmail.com>
2019-03-13 12:58:56 +0300

    test in .ts file

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,13 @@
-<template>
-  <div id="app"></div>
-</template>
-
-<script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import TestStore from './module';
 
 const Super = Vue.extend({
-  methods: TestStore.mapMutations(['setTest'])
+    methods: TestStore.mapMutations(['setTest'])
 });
 
 @Component
 export default class App extends Super {
-  mounted() {
-    this.setTest(true);
-  }
+    mounted() {
+        this.setTest(true);
+    }
 }
-</script>
